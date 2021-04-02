b6e56e3fba2908ce84d2341b7f36461b50baa9ab
JotaRoyal <80458356+JotaRoyal@users.noreply.github.com>
2021-04-03 00:26:17 +0200

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '19728806294', 'Sotravil, m)
-  this.sendContact(m.chat, '19728806294', 'Sotravil', m)
+  // this.sendContact(m.chat, '34644364340', 'JotaRoyal, m)
+  this.sendContact(m.chat, '34644364340', 'JotaRoyal', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
